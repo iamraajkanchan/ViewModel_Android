1de19ec044537b0eb4a9d8e9524dd706303bb221
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-01-01 20:33:20 +0530

    Example of ViewModel

--- app/src/main/java/com/example/viewmodel_android/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel_android/MainActivity.kt
@@ -3,24 +3,26 @@ package com.example.viewmodel_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var counter: Int = 0;
+    private lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         setContent()
     }
 
     /* Do not declare the fun as private because this function is declared in the xml file of the activity*/
     fun increment(view: View?) {
-        counter++
+        mainViewModel.increment()
         setContent()
     }
 
     private fun setContent() {
-        tvCounter.text = counter.toString()
+        tvCounter.text = mainViewModel.counter.toString()
     }
 
 }
--- app/src/main/java/com/example/viewmodel_android/MainViewModel.kt
+++ app/src/main/java/com/example/viewmodel_android/MainViewModel.kt
@@ -3,5 +3,12 @@ package com.example.viewmodel_android
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+    /* Do not make the variable private because you have to use this variable in MainActivity */
+    var counter: Int = 0;
+
+    /* Do not make the function private because you have to use this function in MainActivity */
+    fun increment() {
+        counter++
+    }
 
 }
