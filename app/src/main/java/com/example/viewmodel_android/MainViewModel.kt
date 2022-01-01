7e0604449b30cf36e79dbdffb91f435339f110e7
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-01-01 22:44:43 +0530

    Example of ViewModelFactory

--- app/src/main/java/com/example/viewmodel_android/MainViewModel.kt
+++ app/src/main/java/com/example/viewmodel_android/MainViewModel.kt
@@ -2,10 +2,6 @@ package com.example.viewmodel_android
 
 import androidx.lifecycle.ViewModel
 
-<<<<<<< Updated upstream
-class MainViewModel : ViewModel() {
-
-=======
 class MainViewModel(private val initialValue: Int) : ViewModel() {
     /* Do not make the variable private because you have to use this variable in MainActivity */
     var counter: Int = initialValue;
@@ -14,5 +10,4 @@ class MainViewModel(private val initialValue: Int) : ViewModel() {
     fun increment() {
         counter++
     }
->>>>>>> Stashed changes
 }
